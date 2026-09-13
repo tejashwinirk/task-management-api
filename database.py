41ac09ab8f3c4c52d48bf400de6cdeeebda974c9
tejashwinirk72 <166777811+tejashwinirk72@users.noreply.github.com>
2026-09-13 21:38:39 +0530

    Stage 0: setup server and supabase client

--- database.py
+++ database.py
@@ -1,8 +1,15 @@
 import os
-import psycopg
 from dotenv import load_dotenv
+from supabase import create_client, Client
 
 load_dotenv()
+
+load_dotenv()
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
